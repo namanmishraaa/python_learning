cb80cd03872dcd6ff95320cb2e8f966310ea182f
namanmishraaa <pmishra0850@gmail.com>
2026-08-27 01:26:59 +0530

    Refactor code structure for improved readability and maintainability

---
+++ 12_thread_concurency/07_thread_download.py
@@ -0,0 +1,31 @@
+import threading
+import requests
+import time
+
+
+def download(url):
+    print(f"Starting download... from {url}")
+    resp = requests.get(url=url)
+    print(f"Finished downloading form {url}, size {len(resp.content)} bytes")
+
+urls=[
+    "https://httpbin.org/image/jpeg",
+    "https://httpbin.org/image/png",
+    "https://httpbin.org/image/svg",
+]
+
+start = time.time()
+threads = []
+
+for url in urls:
+    t = threading.Thread(target=download, args=(url,))
+    t.start()
+    threads.append(t)
+
+
+for t in threads:
+    t.join()
+
+end = time.time()
+
+print(f"All download in {end-start:.2f} second")
---
+++ 12_thread_concurency/09_process_one.py
@@ -0,0 +1,22 @@
+import threading 
+import time
+
+def cpu_heavy():
+    print(f"Cruching some numbers ...")
+    total = 0
+    for i in range(10**7):
+        total += i
+    print("Done")
+
+
+
+start = time.time()
+
+threads = [threading.Thread(target=cpu_heavy) for _ in range(2)]
+
+[t.start() for t in threads]
+[t.join() for t in threads]
+
+end = time.time()
+
+print(f"Total time take : {end - start:.2f} second")
