a950f27915d465eef89988770b74cb48062f5b98
namanmishraaa <pmishra0850@gmail.com>
2026-08-30 22:20:54 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Generative_AI/01_tokenization/main.py
@@ -0,0 +1,12 @@
+import tiktoken
+
+enc = tiktoken.encoding_for_model("gpt-4o")
+
+text = "Hey there! My name is Naman Mishra"
+tokens = enc.encode(text)
+
+print("Tokens : ", tokens)
+
+decode = enc.decode([25216, 1354, 0, 3673, 1308, 382, 478, 7601, 147276, 614])
+print("Decoded tokens: ", decode)
+
